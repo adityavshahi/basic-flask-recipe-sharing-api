04930d156ea405b411186ffcdcffc65c0519538c
adityavshahi <avshahi2001@gmail.com>
2023-09-05 13:50:15 +0530

    basic implementations

--- app.py
+++ app.py
@@ -26,4 +26,39 @@ def get_recipe(recipe_id):
     for recipe in recipes:
         if recipe['id'] == int(recipe_id):
             return jsonify(recipe)
-    return jsonify({'message': 'recipe not found .'})
+    return jsonify({'message': 'recipe not found .'}),HTTPStatus.NOT_FOUND
+
+@app.route("/recipes",methods=["POST"])
+def create_recipe():
+    data = request.get_json()
+    name = data.get("name")
+    description = data.get("description")
+    recipe = {
+        'id': len(recipes) + 1,
+        'name': name,
+        'description': description
+    }
+    recipes.append(recipe)
+    return jsonify(recipe),HTTPStatus.CREATED
+
+@app.route("/recipes/<int:recipe_id>",methods=["PUT"])
+def update_recipe(recipe_id):
+    for recipe in recipes:
+        if recipe['id'] == recipe_id:
+            data = request.get_json()
+            recipe.update(
+                {
+                    "name": data.get("name"),
+                    "description": data.get("description")
+                }
+            )
+            return jsonify(recipe)
+    return jsonify({"message": "recipe not found ."}),HTTPStatus.NOT_FOUND
+
+@app.route("/recipes/<int:recipe_id>",methods=["DELETE"])
+def delete_recipe(recipe_id):
+    for recipe in recipes:
+        if recipe['id'] == recipe_id:
+            recipes.remove(recipe)
+            return jsonify({"message": "Recipe Deletion Successful"})
+    return jsonify({"message": "recipe not found ."}),HTTPStatus.NOT_FOUND
